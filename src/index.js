64c791b249611a7143ac6143b1a88cfa36d545f3
Volodymyr Kurov <vokur13@gmail.com>
2022-11-30 19:50:41 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { App } from 'components/App';
-import { store } from './app/store'
+import { store } from './redux/store'
 import { Provider } from 'react-redux'
 import './index.css';
 
